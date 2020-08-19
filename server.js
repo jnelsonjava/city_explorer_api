8e992e0983475998350b560cb47b2ef5ca16e682
Jack Nelson <jnelson.java@gmail.com>
2020-08-18 17:19:10 -0700

    created trails route to serve trail data

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const { response } = require('express');
 const PORT = process.env.PORT || 3003; // default PORT 3003 if .env spec fails
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const TRAIL_API_KEY = process.env.TRAIL_API_KEY;
 const app = express();
 app.use(cors());
 
@@ -45,6 +46,20 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/trails', (req, res) => {
+  const lat = req.query.latitude;
+  const lon = req.query.longitude;
+  const urlToSearch = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${TRAIL_API_KEY}`;
+
+  superagent.get(urlToSearch)
+    .then(resFromSuperagent => {
+      // throw new Error('stuff is broke y\'all');
+      const jsonTrailDataArray = resFromSuperagent.body.trails;
+      res.send(jsonTrailDataArray.map(trailData => new Trail(trailData)));
+    })
+    // .catch(errorHandler)
+})
+
 
 // --- Functions ---
 
@@ -59,6 +74,23 @@ function Weather(forecastObj) {
   this.forecast = forecastObj.weather.description;
   this.time = forecastObj.valid_date;
 }
+
+function Trail(trailObj) {
+  this.name = trailObj.name;
+  this.location = trailObj.location;
+  this.length = trailObj.length;
+  this.stars = trailObj.stars;
+  this.star_votes = trailObj.starVotes;
+  this.summary = trailObj.summary;
+  this.trail_url = trailObj.url;
+  this.conditions = `${trailObj.conditionStatus}: ${trailObj.conditionDetails}`;
+  this.condition_date = trailObj.conditionDate.split(' ')[0];
+  this.condition_time = trailObj.conditionDate.split(' ')[1];
+}
+
+// function errorHandler(error) {
+//   response.status(500).send(error.message);
+// }
 
 
 // --- Server Start ---
