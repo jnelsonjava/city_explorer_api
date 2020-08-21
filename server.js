6df7968169ae7e557356a940bd4f92aaa73219fc
Jack Nelson <jnelson.java@gmail.com>
2020-08-20 20:40:40 -0700

    added route for restaurant queries

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const TRAIL_API_KEY = process.env.TRAIL_API_KEY;
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const YELP_API_KEY = process.env.YELP_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
 const app = express();
 
@@ -33,6 +34,7 @@ app.get('/location', getLocation);
 app.get('/weather', getWeather);
 app.get('/trails', getTrails);
 app.get('/movies', getMovies);
+app.get('/yelp', getYelp);
 
 
 // --- Route Handlers ---
@@ -108,7 +110,6 @@ function getMovies(req, res) {
 
   superagent.get(movieApiUrl)
     .then(movieApiResult => {
-      console.log(movieApiResult.body.results[0])
       res.send(movieApiResult.body.results.map(movie => new Movie(movie)));
     })
     .catch(error => {
@@ -117,6 +118,41 @@ function getMovies(req, res) {
     });
 }
 
+function getYelp(req, res) {
+  console.log(req.query);
+  const limit = 20;
+  const term = 'restaurant';
+  const offset = limit * req.query.page - limit;
+
+  const yelpQuery = `https://api.yelp.com/v3/businesses/search?latitude=${req.query.latitude}&longitude=${req.query.longitude}&limit=${limit}&term=${term}&offset=${offset}`;
+
+  superagent.get(yelpQuery)
+    .set('Authorization', `Bearer ${YELP_API_KEY}`)
+    .then(yelpResponse => {
+      res.send(yelpResponse.body.businesses.map(restaurant => new Yelp(restaurant)));
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send(error.message);
+    });
+
+  // res.send([
+  //   {
+  //     "name": "Pike Place Chowder",
+  //     "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
+  //     "price": "$$   ",
+  //     "rating": "4.5",
+  //     "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
+  //   },
+  //   {
+  //     "name": "Umi Sake House",
+  //     "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/c-XwgpadB530bjPUAL7oFw/o.jpg",
+  //     "price": "$$   ",
+  //     "rating": "4.0",
+  //     "url": "https://www.yelp.com/biz/umi-sake-house-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
+  //   }
+  // ])
+}
 
 
 // --- Functions ---
@@ -151,26 +187,53 @@ function Movie(movieObj) {
   this.overview = movieObj.overview;
   this.average_votes = movieObj.vote_average;
   this.total_votes = movieObj.vote_count;
-  this.image_url = movieObj.poster_path;
+  this.image_url = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + movieObj.poster_path;
   this.popularity = movieObj.popularity;
   this.released_on = movieObj.release_date;
 }
 
+function Yelp(yelpObj) {
+  this.name = yelpObj.name;
+  this.image_url = yelpObj.image_url;
+  this.price = yelpObj.price;
+  this.rating = yelpObj.rating;
+  this.url = yelpObj.url;
+}
+
 // {
-//   popularity: 9.114,
-//   vote_count: 250,
-//   video: false,
-//   poster_path: '/AopGrtgNJFj04oCXgKMwvUfF0hg.jpg',
-//   id: 12637,
-//   adult: false,
-//   backdrop_path: '/dy9vvNddvAdsKXKnulnvhP8FxGv.jpg',
-//   original_language: 'en',
-//   original_title: 'New York, New York',
-//   genre_ids: [ 18, 10402, 10749 ],
-//   title: 'New York, New York',
-//   vote_average: 6.6,
-//   overview: 'An egotistical saxophone player and a young singer meet on V-J Day and embark upon a strained and rocky romance, even as their careers begin a long uphill climb.',
-//   release_date: '1977-06-21'
+//   id: '6I28wDuMBR5WLMqfKxaoeg',
+//   alias: 'pike-place-chowder-seattle',
+//   name: 'Pike Place Chowder',
+//   image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/ZyQjV-wJQ2GHyX7l3jfbyg/o.jpg',
+//   is_closed: false,
+//   url: 'https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=5fnHnfOzM-sMwBOk--Xvfg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=5fnHnfOzM-sMwBOk--Xvfg',
+//   review_count: 7356,
+//   categories: [ [Object], [Object] ],
+//   rating: 4.5,
+//   coordinates: { latitude: 47.60939, longitude: -122.34112 },
+//   transactions: [ 'pickup', 'delivery' ],
+//   price: '$$',
+//   location: {
+//     address1: '1530 Post Aly',
+//     address2: 'Ste 11',
+//     address3: '',
+//     city: 'Seattle',
+//     zip_code: '98101',
+//     country: 'US',
+//     state: 'WA',
+//     display_address: [Array]
+//   },
+//   phone: '+12062672537',
+//   display_phone: '(206) 267-2537',
+//   distance: 1832.5436169291488
+// }
+
+// {
+//   "name": "Pike Place Chowder",
+//   "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
+//   "price": "$$   ",
+//   "rating": "4.5",
+//   "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
 // }
 
 
